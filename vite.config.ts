1cd2aa41ccef60936ef9841a84cb3be42b6e18cb
Monster <1226189767@qq.com>
2025-01-23 16:24:58 +0800

    feat: 更新md

--- vite.config.ts
+++ vite.config.ts
@@ -44,19 +44,23 @@ export default defineConfig({
   },
   server: {
     port: env.VITE_PORT ? +env.VITE_PORT : 5173,
-    open: true,
+    host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:7001',
+        // target: 'http://localhost:7001',
+        // http://172.31.221.8:7001
+        target: 'http://123.56.101.254:7001',
         changeOrigin: true,
       },
       '/file': {
-        target: 'http://localhost:9002',
+        target: 'http://123.56.101.254:9002',
+        // target: 'http://localhost:9002',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/file/, ''),
       },
       '/ws': {
-        target: 'ws://localhost:7001',
+        target: 'ws://123.56.101.254:7001',
+        // target: 'ws://localhost:7001',
         changeOrigin: true,
         ws: true,
         rewrite: (path) => path.replace(/^\/ws/, ''),
